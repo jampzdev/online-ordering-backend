803b787b5bf1e56c286d5d6f57aad6ad72f86922
Jampz <jampz.jampolina@musictribe.com>
2021-05-24 15:21:12 +0800

    db

--- db/db.sql
+++ db/db.sql
@@ -18,6 +18,8 @@ USE `online_ordering_db`;
 
 /*Table structure for table `admin_tbl` */
 
+DROP TABLE IF EXISTS `admin_tbl`;
+
 CREATE TABLE `admin_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `fname` varchar(50) DEFAULT NULL,
@@ -27,9 +29,11 @@ CREATE TABLE `admin_tbl` (
   `passhash` varchar(200) DEFAULT NULL,
   `is_active` enum('0','1') DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `brands_tbl` */
+
+DROP TABLE IF EXISTS `brands_tbl`;
 
 CREATE TABLE `brands_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -39,6 +43,8 @@ CREATE TABLE `brands_tbl` (
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `categories_tbl` */
+
+DROP TABLE IF EXISTS `categories_tbl`;
 
 CREATE TABLE `categories_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -50,6 +56,8 @@ CREATE TABLE `categories_tbl` (
 
 /*Table structure for table `inventory_tbl` */
 
+DROP TABLE IF EXISTS `inventory_tbl`;
+
 CREATE TABLE `inventory_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `product_id` bigint(20) DEFAULT NULL,
@@ -60,6 +68,8 @@ CREATE TABLE `inventory_tbl` (
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `orders_products_tbl` */
+
+DROP TABLE IF EXISTS `orders_products_tbl`;
 
 CREATE TABLE `orders_products_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -74,6 +84,8 @@ CREATE TABLE `orders_products_tbl` (
 
 /*Table structure for table `orders_tbl` */
 
+DROP TABLE IF EXISTS `orders_tbl`;
+
 CREATE TABLE `orders_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
@@ -85,6 +97,8 @@ CREATE TABLE `orders_tbl` (
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `product_colors_tbl` */
+
+DROP TABLE IF EXISTS `product_colors_tbl`;
 
 CREATE TABLE `product_colors_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -98,6 +112,8 @@ CREATE TABLE `product_colors_tbl` (
 
 /*Table structure for table `product_sizes_tbl` */
 
+DROP TABLE IF EXISTS `product_sizes_tbl`;
+
 CREATE TABLE `product_sizes_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `product_id` bigint(20) DEFAULT NULL,
@@ -107,6 +123,8 @@ CREATE TABLE `product_sizes_tbl` (
 ) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `product_varieties_tbl` */
+
+DROP TABLE IF EXISTS `product_varieties_tbl`;
 
 CREATE TABLE `product_varieties_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -122,6 +140,8 @@ CREATE TABLE `product_varieties_tbl` (
 
 /*Table structure for table `products_tbl` */
 
+DROP TABLE IF EXISTS `products_tbl`;
+
 CREATE TABLE `products_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(150) DEFAULT NULL,
@@ -134,6 +154,8 @@ CREATE TABLE `products_tbl` (
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `users_tbl` */
+
+DROP TABLE IF EXISTS `users_tbl`;
 
 CREATE TABLE `users_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -149,6 +171,8 @@ CREATE TABLE `users_tbl` (
 
 /*Table structure for table `varieties_tbl` */
 
+DROP TABLE IF EXISTS `varieties_tbl`;
+
 CREATE TABLE `varieties_tbl` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `variety_name` varchar(150) DEFAULT NULL,
@@ -161,7 +185,9 @@ CREATE TABLE `varieties_tbl` (
 
 DELIMITER $$
 
-/*!50003 CREATE */ /*!50017 DEFINER = 'teamlms'@'localhost' */ /*!50003 TRIGGER `setCurrentDateOrders` BEFORE INSERT ON `orders_tbl` FOR EACH ROW  
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `setCurrentDateOrders` */$$
+
+/*!50003 CREATE */ /*!50003 TRIGGER `setCurrentDateOrders` BEFORE INSERT ON `orders_tbl` FOR EACH ROW  
  set New.date_created = Now() */$$
 
 
@@ -171,7 +197,9 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE */ /*!50017 DEFINER = 'root'@'localhost' */ /*!50003 TRIGGER `prodNewColor` BEFORE INSERT ON `product_colors_tbl` FOR EACH ROW 
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `prodNewColor` */$$
+
+/*!50003 CREATE */ /*!50003 TRIGGER `prodNewColor` BEFORE INSERT ON `product_colors_tbl` FOR EACH ROW 
 	SET New.size_id = (SELECT id FROM product_sizes_tbl WHERE product_id = New.product_id AND size_name = New.size_name) */$$
 
 
@@ -181,7 +209,9 @@ DELIMITER ;
 
 DELIMITER $$
 
-/*!50003 CREATE */ /*!50017 DEFINER = 'root'@'localhost' */ /*!50003 TRIGGER `prodNewColorAfter` AFTER INSERT ON `product_colors_tbl` FOR EACH ROW 
+/*!50003 DROP TRIGGER*//*!50032 IF EXISTS */ /*!50003 `prodNewColorAfter` */$$
+
+/*!50003 CREATE */ /*!50003 TRIGGER `prodNewColorAfter` AFTER INSERT ON `product_colors_tbl` FOR EACH ROW 
 	INSERT INTO inventory_tbl(product_id,product_size,product_color)VALUES(New.product_id,New.size_id,new.id) */$$
 
 
@@ -190,6 +220,9 @@ DELIMITER ;
 /*Table structure for table `inventory_view` */
 
 DROP TABLE IF EXISTS `inventory_view`;
+
+/*!50001 DROP VIEW IF EXISTS `inventory_view` */;
+/*!50001 DROP TABLE IF EXISTS `inventory_view` */;
 
 /*!50001 CREATE TABLE  `inventory_view`(
  `id` bigint(20) ,
@@ -211,6 +244,9 @@ DROP TABLE IF EXISTS `inventory_view`;
 
 DROP TABLE IF EXISTS `products_view`;
 
+/*!50001 DROP VIEW IF EXISTS `products_view` */;
+/*!50001 DROP TABLE IF EXISTS `products_view` */;
+
 /*!50001 CREATE TABLE  `products_view`(
  `id` bigint(20) ,
  `product_name` varchar(150) ,
@@ -227,12 +263,16 @@ DROP TABLE IF EXISTS `products_view`;
 /*View structure for view inventory_view */
 
 /*!50001 DROP TABLE IF EXISTS `inventory_view` */;
-/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `inventory_view` AS (select `a`.`id` AS `id`,`a`.`product_name` AS `product_name`,`a`.`product_desc` AS `product_desc`,`a`.`category_id` AS `category_id`,`a`.`brand_id` AS `brand_id`,`e`.`brand_name` AS `brand_name`,`a`.`img` AS `img`,`f`.`category_name` AS `category_name`,`c`.`size_name` AS `size_name`,`d`.`color_name` AS `color_name`,`c`.`price` AS `price`,`b`.`quantity` AS `quantity`,`a`.`is_active` AS `is_active` from (((((`inventory_tbl` `b` join `products_tbl` `a` on((`a`.`id` = `b`.`product_id`))) join `product_sizes_tbl` `c` on((`c`.`id` = `b`.`product_size`))) join `product_colors_tbl` `d` on((`d`.`id` = `b`.`product_color`))) join `brands_tbl` `e` on((`e`.`id` = `a`.`brand_id`))) join `categories_tbl` `f` on((`f`.`id` = `a`.`category_id`)))) */;
+/*!50001 DROP VIEW IF EXISTS `inventory_view` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `inventory_view` AS (select `a`.`id` AS `id`,`a`.`product_name` AS `product_name`,`a`.`product_desc` AS `product_desc`,`a`.`category_id` AS `category_id`,`a`.`brand_id` AS `brand_id`,`e`.`brand_name` AS `brand_name`,`a`.`img` AS `img`,`f`.`category_name` AS `category_name`,`c`.`size_name` AS `size_name`,`d`.`color_name` AS `color_name`,`c`.`price` AS `price`,`b`.`quantity` AS `quantity`,`a`.`is_active` AS `is_active` from (((((`inventory_tbl` `b` join `products_tbl` `a` on((`a`.`id` = `b`.`product_id`))) join `product_sizes_tbl` `c` on((`c`.`id` = `b`.`product_size`))) join `product_colors_tbl` `d` on((`d`.`id` = `b`.`product_color`))) join `brands_tbl` `e` on((`e`.`id` = `a`.`brand_id`))) join `categories_tbl` `f` on((`f`.`id` = `a`.`category_id`)))) */;
 
 /*View structure for view products_view */
 
 /*!50001 DROP TABLE IF EXISTS `products_view` */;
-/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `products_view` AS (select `a`.`id` AS `id`,`a`.`product_name` AS `product_name`,`a`.`product_desc` AS `product_desc`,`a`.`category_id` AS `category_id`,`a`.`brand_id` AS `brand_id`,`b`.`brand_name` AS `brand_name`,`a`.`img` AS `img`,`c`.`category_name` AS `category_name`,(select group_concat(concat(`d`.`size_name`,'|',`d`.`price`,'-',(select group_concat(concat(`e`.`color_name`,'~',`e`.`add_price`) separator '/') from `product_colors_tbl` `e` where (`e`.`size_id` = `d`.`id`))) separator ',') from `product_sizes_tbl` `d` where (`d`.`product_id` = `a`.`id`)) AS `sizes`,`a`.`is_active` AS `is_active` from ((`products_tbl` `a` join `brands_tbl` `b` on((`b`.`id` = `a`.`brand_id`))) join `categories_tbl` `c` on((`c`.`id` = `a`.`category_id`)))) */;
+/*!50001 DROP VIEW IF EXISTS `products_view` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `products_view` AS (select `a`.`id` AS `id`,`a`.`product_name` AS `product_name`,`a`.`product_desc` AS `product_desc`,`a`.`category_id` AS `category_id`,`a`.`brand_id` AS `brand_id`,`b`.`brand_name` AS `brand_name`,`a`.`img` AS `img`,`c`.`category_name` AS `category_name`,(select group_concat(concat(`d`.`size_name`,'|',`d`.`price`,'-',(select group_concat(concat(`e`.`color_name`,'~',`e`.`add_price`) separator '/') from `product_colors_tbl` `e` where (`e`.`size_id` = `d`.`id`))) separator ',') from `product_sizes_tbl` `d` where (`d`.`product_id` = `a`.`id`)) AS `sizes`,`a`.`is_active` AS `is_active` from ((`products_tbl` `a` join `brands_tbl` `b` on((`b`.`id` = `a`.`brand_id`))) join `categories_tbl` `c` on((`c`.`id` = `a`.`category_id`)))) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
